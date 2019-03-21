c5e2ffc2255648c08014b46d67120f8abfe3e2ae
saicharan96 <saicharan14996@gmail.com>
2019-03-21 17:05:23 +0530

    sss
    sssss

---
+++ assets/js/sidenav.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(1000).css({'overflow':'visible'});
+})
+
+
+ 
